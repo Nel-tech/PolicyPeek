99e2032727f909fe077515c58c2d05ae4a771c85
Nel-tech <favournelson82@gmail.com>
2025-07-05 17:10:13 +0100

    Update GoogleButton component to use OpenSans font for improved typography consistency

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -14,14 +14,14 @@ export default function GoogleLoginButton({text}: GoogleProps) {
         <div className="flex justify-center">
             <button
                 onClick={handleGoogleLogin}
-                className="w-64 flex items-center cursor-pointer justify-center gap-3 bg-white border border-gray-300 px-6 py-3 rounded shadow hover:shadow-md"
+                className="w-64 font-sans flex items-center cursor-pointer justify-center gap-3 bg-white border border-gray-300 px-6 py-3 rounded shadow hover:shadow-md"
             >
                 <img
                     src="https://developers.google.com/identity/images/g-logo.png"
                     alt="Google logo"
                     className="w-5 h-5"
                 />
-                <span className="text-sm font-medium text-gray-700">
+                <span className="text-sm font-sans font-medium text-gray-700">
                   {text}
                 </span>
             </button>
